fc1aa60c55e93f0cfafa3a4abc75db05863e8269
Shadman Ahmed <shadman@outlook.com>
2020-05-15 20:11:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,18 +6,18 @@ def using_insert(array, element)
   array.insert(element)
 end
 
-def using_uniq
-
+def using_uniq(array)
+  array.uniq
 end
 
-def using_flatten
-
+def using_flatten(array)
+  array.flatten
 end
 
-def using_delete
-
+def using_delete(array,string)
+  array.delete(string)
 end
 
-def using_delete_at
-
+def using_delete_at(array,integer)
+  array.delete_at[integer]
 end
