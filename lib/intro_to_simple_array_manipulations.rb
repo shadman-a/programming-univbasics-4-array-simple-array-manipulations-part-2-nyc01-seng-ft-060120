5bb18a137d14caa1769b7350a6868141dd835f4d
Shadman Ahmed <shadman@outlook.com>
2020-05-15 20:09:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,13 +1,13 @@
 def using_concat(array1,array2)
-array1.concat(array2)
+  array1.concat(array2)
 end
 
-def using_insert
-
+def using_insert(array, element)
+  array.insert(element)
 end
 
 def using_uniq
-
+  
 end
 
 def using_flatten
